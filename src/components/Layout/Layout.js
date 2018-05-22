ff0efeda26b4eb2cb52f7ccf6c4d2891ba9b85ea
Ryan <r.l.wells@outlook.com>
2018-05-22 10:16:22 -0700

    nav components

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,10 +2,11 @@ import React from 'react';
 
 import Aux from '../../hoc/Auxx';
 import classes from './Layout.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 
 const layout = ( props ) => (
     <Aux>
-        <div>Toolbar, SiteDrawer, Backdrop</div>
+        <Toolbar />
         <main className={classes.Content}>
             {props.children}
         </main>
