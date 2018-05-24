4cb96da8639a935e14d208cd24a60b5b0c3343c2
Ryan <r.l.wells@outlook.com>
2018-05-24 12:44:06 -0700

    added order fetch functionality

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -93,27 +93,20 @@ class BurgerBuilder extends Component {
 
     purchaseContinueHandler = () => {
         // alert('You Continue!');
-        this.setState({loading: true});
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Max',
-                address: {
-                    street: 'teststreet 1',
-                    zipCode: '86753',
-                    country: 'U.S.'
-                },
-                email: 'example@example.com'
-            },
-            deliveryMethod: 'fastest'
+
+    const queryParams = [];
+        for (let i in this.state.ingredients) {
+            queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
         }
-        axios.post('/orders.json', order)//firebase needs .json to fire correctly
-            .then(response => { this.setState( { loading: false, purchasing: false } );
-            })
-            .catch(error => {
-                this.setState( { loading: false, purchasing: false } );
-            });
+        
+    queryParams.push('price=' + this.state.totalPrice);
+
+    const queryString = queryParams.join('&');
+
+    this.props.history.push({
+        pathname: '/checkout',
+        search: '?' + queryString
+    });
     }
 
     render () {
