4ff9f0e70b9afc50b33325bcb3873eb280b30e6d
Ryan <r.l.wells@outlook.com>
2018-06-04 13:22:31 -0700

    added firebase deployment and unit testing

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -12,7 +12,7 @@ import * as actions from '../../store/actions/index'; //index can be ommitted
 import burger from '../../components/Burger/Burger';
 import axios from '../../axios-orders';
 
-class BurgerBuilder extends Component {
+export class BurgerBuilder extends Component {
     // constructor(props) {
     //     super(props);
     //     this.state = {...}
