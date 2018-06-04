ec89100b5b4d76a2bd927814f193f6c3b3cb5209
Ryan <r.l.wells@outlook.com>
2018-06-03 17:23:54 -0700

    added authentication and security features

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -23,7 +23,7 @@ const buildControls = (props) => (
         ))}
         <button className={classes.OrderButton}
         disabled={!props.purchaseable}
-        onClick={props.ordered}>Order Now</button>
+        onClick={props.ordered}>{props.isAuth ? 'Order Now' : 'Sign Up To Order'}</button>
     </div>
 );
 
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -102,10 +102,11 @@ class ContactData extends Component {
         const order = {
             ingredients: this.props.ings,
             price: this.props.price,
-            orderData: formData
+            orderData: formData,
+            userId: this.props.userId
         }
 
-        this.props.onOrderBurger(order);
+        this.props.onOrderBurger(order, this.props.token);
         
     }
 
@@ -199,13 +200,15 @@ const mapStateToProps = state => {
     return {
         ings: state.burgerBuilder.ingredients,
         price: state.burgerBuilder.totalPrice,
-        loading: state.order.loading
+        loading: state.order.loading,
+        token: state.auth.token,
+        userId: this.state.userId
     }
 };
 
 const mapDispatchToProps = dispatch => {
     return {
-        onOrderBurger: (orderData) => dispatch(actions.purchaseBurger(orderData))
+        onOrderBurger: (orderData, token) => dispatch(actions.purchaseBurger(orderData, token))
     };
     
 }
